d2e11a5bbd8c79a0b7beaa622564680c4d8df517
Nehemiah-o <67313020+Nehemiah-o@users.noreply.github.com>
2020-10-02 21:35:08 +0300

    publishing my firsrt web
    am happy

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Ondichor254</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>
+    ondichor proffessor
+  </h1>
+  <p>ebhfeyiclebjivbebvuibri gbri</p>
+</div>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/4.jpg" class="img-fluid aboutimg">
+    </div>
+<div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">Am Ondicho From Kisii</h2>
+      <p class="py-3">The idae okodnvjoduv ocndveyvdkcvw dcnekckwvcvevuuw dcekyuecuveukwvcu dje wehbchvc dhcydvh dbcuecdvukduhcvsukvdcud dcvyudvcyudvyudvyucvducvdc dbvduvcydgydudvhvdyubvdb hdv tudh dhk vdyuv duvidv hd</p>
+      <a href="about.php" class="btn-outline-success">Check Mores</a>
+    </div>
+  </div>
+</div>
+
+  
+</section>
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@nehemiahondicho</p>
+    
+
+
+  </footer>
+
+
+
+
+
+
+</body>
+</html>
---
+++ userinfo.php
@@ -0,0 +1,34 @@
+<?php
+$con = mysqli_connect("localhost","root");
+if ($con) {
+	echo "connection successfull";
+} else {
+	echo "No connection";
+}
+mysqli_select_db($con, "ondichor1data");
+$user = $_POST['user'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$comments = $_POST['comments'];
+
+$query = " insert into userinfodata (user, email, mobile, comment)
+values ('$user','$email','$mobile','$comments')";
+echo "$query";
+mysqli_query($con, $query);
+header('location:index.php');
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
